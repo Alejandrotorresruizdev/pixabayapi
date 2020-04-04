6683d43d0fbe72970239d3ffe228b26a39c628f4
OpenCLoud <alejandro.torres@opencloud.es>
2020-04-04 12:40:49 +0200

    Updates

---
+++ precache-manifest.826025fac9db827abc72b47af7ea66da.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15fbdff6fb5ef1209b4a3e9b2e71d04f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "00dc4f0ec46b4f09e2c8",
+    "url": "/static/js/2.6723a1ec.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6723a1ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76e7f3d620f34588871b",
+    "url": "/static/js/main.1d293052.chunk.js"
+  },
+  {
+    "revision": "b88bf9c2baf283690e67",
+    "url": "/static/js/runtime-main.e259c6ef.js"
+  }
+]);
